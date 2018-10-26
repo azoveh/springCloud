a51bb3d3d7f7363225232b4b45a46530c3d42a12
Aihua <zhangaihua@inchain.org>
2018-10-26 18:27:06 +0800

    update

--- feign/service-discovery/src/main/java/io/twostepsfromjava/cloud/service/discovery/Application.java
+++ feign/service-discovery/src/main/java/io/twostepsfromjava/cloud/service/discovery/Application.java
@@ -28,6 +28,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class Application {
 
+
+
+
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class).web(true).run(args);
     }
